b1b14d321945ee9a82ec2445ad955af688f4adb4
Unnati Jain <36570409+Unnati111@users.noreply.github.com>
2021-01-10 20:06:08 +0530

    Test case 4 passes

--- StringCalculator.java
+++ StringCalculator.java
@@ -4,17 +4,24 @@ public class StringCalculator{
 	public int Add(String numbers){
 		int sum=0;
 		String[] arrSplit = null;
-
+		String delimiter=null,newstring=null;
 		if(numbers.length()==0) {   //Test Case1 passed
 			return 0;
 		}
 		else {
-			arrSplit = numbers.split(",|\\n");
-			for (int i=0; i < arrSplit.length; i++)
-			{
+			if(numbers.startsWith("//")) {
+					delimiter=Character.toString(numbers.charAt(2));
+					newstring=numbers.substring(numbers.indexOf("\n")+1);
+					arrSplit = newstring.split(delimiter.trim());
+				}
+				else{
+					arrSplit = numbers.split(",|\\n");
+				}
+			 for (int i=0; i < arrSplit.length; i++){
 				int temp=Integer.parseInt(arrSplit[i].trim());
 				sum+=temp;
-			}
+			 }
+
 			return sum;
 		}
 	}
@@ -26,10 +33,12 @@ public class StringCalculator{
 		int value2=s.Add("1,2");
 		int value3=s.Add("1,2,3,4,51,9"); //Test Case 2 passed
 		int value4=s.Add("1\n2,3"); //Test Case 3 passed
+		int value5=s.Add("//;\n1;2;7");//Test Case 4 passed
 	  System.out.println("Input:''  Output:"+value);
 	  System.out.println("Input:'1'  Output:"+value1);
 		System.out.println("Input:'1,2'  Output:"+value2);
 		System.out.println("Input:'1,2,3,4,51,9'  Output:"+value3);
 		System.out.println("Input:'1\\n2,3'  Output:"+value4);
+		System.out.println("Input:'//;\\n1;2;7'  Output:"+value5);
 	}
 }

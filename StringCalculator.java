502b6aaac39b9adda4f18400a5f7a8ecee7b2048
Unnati Jain <36570409+Unnati111@users.noreply.github.com>
2021-01-10 19:26:51 +0530

    First testcase Passes

---
+++ StringCalculator.java
@@ -0,0 +1,31 @@
+
+public class StringCalculator{
+
+	public int Add(String numbers){
+		int sum=0;
+		String[] arrSplit = null;
+
+		if(numbers.length()==0) {   //Test Case1 passed
+			return 0;
+		}
+		else {
+			arrSplit = numbers.split(",");
+			for (int i=0; i < arrSplit.length; i++)
+			{
+				int temp=Integer.parseInt(arrSplit[i].trim());
+				sum+=temp;
+			}
+			return sum;
+		}
+	}
+
+	public static void main(String[] args){
+		StringCalculator s=new StringCalculator();
+		int value=s.Add("");
+		int value1=s.Add("1");
+		int value2=s.Add("1,2");
+	  System.out.println("Input:''  Output:"+value);
+	  System.out.println("Input:'1'  Output:"+value1);
+		System.out.println("Input:'1,2'  Output:"+value2);
+	}
+}
